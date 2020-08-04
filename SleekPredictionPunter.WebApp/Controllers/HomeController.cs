fb7e822ad1feb6912e66fd85811e5f1b2802510d
Daniel Ibanga <danielibanga4321@outlook.com>
2020-08-04 22:34:15 +0100

    ..

--- SleekPredictionPunter.WebApp/Controllers/HomeController.cs
+++ SleekPredictionPunter.WebApp/Controllers/HomeController.cs
@@ -16,13 +16,17 @@ namespace SleekPredictionPunter.WebApp.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly IPredictionService _predictionService;
+		private readonly IPredictorService _predictorService;
+		private readonly IContactAppService _contactService;
 
-        public HomeController(ILogger<HomeController> logger, IPredictionService predictionService, IPredictorService predictorService)
+        public HomeController(ILogger<HomeController> logger, IPredictionService predictionService, IPredictorService predictorService, IContactAppService contactService)
         {
             _logger = logger;
             _predictionService = predictionService;
             _predictorService = predictorService;
-        }
+			_contactService = contactService;
+		}
 
         public async Task<IActionResult> Index()
 		{
