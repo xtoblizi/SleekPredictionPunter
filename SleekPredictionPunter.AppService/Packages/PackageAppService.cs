0e8b406ad78c908a1f6729a569e55817b15e2669
Stanley Ibekwe <sibekwe@coure-tech.com>
2020-08-05 20:08:58 +0100

    commit update

---
+++ SleekPredictionPunter.AppService/Packages/PackageAppService.cs
@@ -0,0 +1,43 @@
+﻿using SleekPredictionPunter.Model.Packages;
+using SleekPredictionPunter.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleekPredictionPunter.AppService.Packages
+{
+    public class PackageAppService : IPackageAppService
+    {
+        private readonly IBaseRepository<Package> _repo;
+        public PackageAppService(IBaseRepository<Package> package)
+        {
+            _repo = package;
+        }
+
+        public async Task Delete(Package model)
+        {
+            await _repo.Delete(model, true);
+        }
+
+        public async Task<Package> GetPackageById(long id)
+        {
+            return await _repo.GetById(id);
+        }
+
+        public async Task<IEnumerable<Package>> GetPackages()
+        {
+            return await _repo.GetAllQueryable();
+        }
+
+        public async Task<long> Insert(Package model)
+        {
+            return await _repo.Insert(model, true);
+        }
+
+        public async Task Update(Package model)
+        {
+            await _repo.Update(model, true);
+        }
+    }
+}
---
+++ SleekPredictionPunter.Model/Packages/PackageDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SleekPredictionPunter.Model.Packages
+{
+    public class PackageDto:Package
+    {
+    }
+}
