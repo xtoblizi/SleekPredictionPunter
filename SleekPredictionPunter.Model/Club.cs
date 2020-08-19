0be60e19de046e99cc0c96e9d89bc632dd989ab0
Ogbosuka Christopher <ogbosukachris@gmail.com>
2020-08-19 14:34:19 +0100

    creation  of club and oddcategory models

---
+++ SleekPredictionPunter.Model/Club.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SleekPredictionPunter.Model
+{
+	public class ClubDto 
+	{
+		public string ClubName { get; set; }
+		public string Description { get; set; }
+		public IFormFile ClubLogo { get; set; }
+	}
+
+	public class Club : BaseEntity
+	{
+		public string ClubName { get; set; }
+		public string Description { get; set; }
+		public  string ClubLogRelativePath { get; set; }
+	}
+}
---
+++ SleekPredictionPunter.Model/OddCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SleekPredictionPunter.Model
+{
+	public class OddCategory: BaseEntity
+	{
+		public string CategoryName { get; set; }
+		public string OddValue { get; set; }
+	}
+}
