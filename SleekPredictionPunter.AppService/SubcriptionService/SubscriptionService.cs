6034595f4fe8d1df96cc4c672c4005d8ea61e9cd
Ogbosuka Christopher <ogbosukachris@gmail.com>
2020-08-23 05:28:25 +0100

    commit

---
+++ SleekPredictionPunter.AppService/SubcriptionService/SubscriptionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SleekPredictionPunter.AppService.SubcriptionService
+{
+	public class SubscriptionService
+	{
+
+	}
+}
