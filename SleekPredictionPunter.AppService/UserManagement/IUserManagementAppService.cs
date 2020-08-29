053248e6998801b8f4a4b5057fa7bf1b97bf51f2
Daniel Ibanga <danielibanga4321@outlook.com>
2020-08-29 17:18:55 +0100

    Get usersmanagement details base on user details

---
+++ SleekPredictionPunter.AppService/UserManagement/IUserManagementAppService.cs
@@ -0,0 +1,14 @@
+﻿using SleekPredictionPunter.Model.TransactionLogs;
+using SleekPredictionPunter.Model.Wallets;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SleekPredictionPunter.AppService.UserManagement
+{
+    public interface IUserManagementAppService
+    {
+        Task<WalletModel> GetUserWalletDetails(Func<WalletModel, bool> predIcate);
+        Task<IEnumerable<TransactionLogModel>> UserLogs(Func<TransactionLogModel, bool> predicate, int skip = 0, int take = int.MaxValue);
+    }
+}
---
+++ SleekPredictionPunter.AppService/UserManagement/UserManagementAppService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using SleekPredictionPunter.AppService.TransactionLog;
+using SleekPredictionPunter.AppService.Wallet;
+using SleekPredictionPunter.Model.TransactionLogs;
+using SleekPredictionPunter.Model.Wallets;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace SleekPredictionPunter.AppService.UserManagement
+{
+    public class UserManagementAppService : IUserManagementAppService
+    {
+        private readonly ITransactionLogAppService _transactionLogAppService;
+        private readonly IWalletAppService _walletAppService;
+        public UserManagementAppService(IWalletAppService walletAppService, ITransactionLogAppService transactionLogAppService)
+        {
+            _transactionLogAppService = transactionLogAppService;
+            _walletAppService = walletAppService;
+        }
+
+        public async Task<IEnumerable<TransactionLogModel>> UserLogs(Func<TransactionLogModel, bool> predicate, int skip = 0, int take = int.MaxValue)
+        {
+            var getAllLogs = await _transactionLogAppService.GetAllTransactionLog(predicate, skip, take);
+            return getAllLogs;
+        }
+        public async Task<WalletModel> GetUserWalletDetails(Func<WalletModel, bool> predIcate)
+        {
+            var result = await _walletAppService.GetByUserId(predIcate);
+            return result != null ? result : null;
+        }
+    }
+}
